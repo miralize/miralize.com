e105745c502a771410b71cdb14b175fb4f9867ea
Sean O'Grady <sean@miralize.com>
2020-07-29 23:12:27 +0100

    PR updates - Remove refs to Babel - Remove vue cli plugins - Remove more unused packages

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   moduleFileExtensions: [
     'js',
-    'jsx',
-    'json',
     'vue',
   ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-    '^.+\\.jsx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
     '/node_modules/',
@@ -23,5 +20,4 @@ module.exports = {
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
   ],
   testURL: 'http://localhost/',
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
 };
